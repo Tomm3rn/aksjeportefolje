52a16fd0bd433c4e055c36fb51d34ef6e57a0fd9
Tomm3rn <github@tomm3rn.com>
2026-03-26 22:00:05 +0100

    fix: rename User table to 'users' to avoid PostgreSQL reserved word

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ with app.app_context():
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
